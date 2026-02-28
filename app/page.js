6df4895735a75a3d1d6263c08f2348b2dace64e0
Samarth Sharma <samarthsharma351@gmail.com>
2026-02-28 15:34:24 -0800

    chore: Adds a working send button

--- app/page.js
+++ app/page.js
@@ -24,6 +24,7 @@ export default function Home() {
 
       const data = await response.json();
       // .content because look at return in route.ts
+      console.log(data);
       setResponse(data.content);
     } catch (error) {
       setResponse("Error: " + error.message);
@@ -33,8 +34,25 @@ export default function Home() {
   };
 
   return (
-    <div className="flex min-h-screen  justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hello Ji
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h2 className="text-2xl font-semibold mb-4">Hello Ji</h2>
+
+      <div className="w-full max-w-xl">
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="What's on your mind?"
+          rows={6}
+          className="w-full border rounded-lg p-4"
+        />
+      </div>
+      <div className="m-4">
+        {!loading ? (
+          <button onClick={handleChat}>Send</button>
+        ) : (
+          <h4>Loading</h4>
+        )}
+      </div>
     </div>
   );
 }
