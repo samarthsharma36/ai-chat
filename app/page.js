703ad09ca222a955d7f7cbc0b948251a437ca121
Samarth Sharma <samarthsharma351@gmail.com>
2026-02-28 14:43:23 -0800

    chore: Adds a handle chat method

--- app/page.js
+++ app/page.js
@@ -1,8 +1,39 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+  const [streamResponse, setStreamResponse] = useState("");
+  const [streaming, setStreaming] = useState("");
+  const [loading, setLoading] = useState("");
+
+  const handleChat = async () => {
+    setLoading(true);
+    setResponse("");
+
+    try {
+      const response = await fetch("/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message }),
+      });
+
+      const data = await response.json();
+      // .content because look at return in route.ts
+      setResponse(data.content);
+    } catch (error) {
+      setResponse("Error: " + error.message);
+    }
+
+    setLoading(false);
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen  justify-center bg-zinc-50 font-sans dark:bg-black">
       Hello Ji
     </div>
   );
